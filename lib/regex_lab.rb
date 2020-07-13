20413667041d1b67d1cc966ddc7f3546a737e97f
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-13 20:19:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ end
 
 def words_five_letters_long(text)
   words = text.split(" ")
-  text.grep(/\w+{5})
+  text.grep(/\b\{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
